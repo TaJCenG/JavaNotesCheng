b7916e7f2c0c458afab80992193923a70a145854
TaJCenG <tajusyed765@gmail.com>
2024-06-28 11:52:19 +0530

    Add files via upload
    updated immuatble string

---
+++ String/String/FirstNotes.java
@@ -0,0 +1,52 @@
+package String;
+
+public class FirstNotes {
+	/*
+	 * String : is treated as object
+	 *          memory allocated in heap area
+	 *          
+	 * Collections of characters which are enclosed within double quotes   "Taj"  "Ceng"
+	 * 
+	 * 2-Types
+	 * =======
+	 * Immutable String
+	 * ----------------
+	 * Something cannot be changed once its created
+	 * Ex:- ourname, fatherName
+	 * Inbuilt class
+	 * 
+	 * Mutable String
+	 * --------------
+	 * can be changed after creating also
+	 * To manage mutable string  java provide  
+	 * String buffer and   String Builder
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * ==                 > reference will be compared
+	 * equals()           > values of string compared
+	 * equalsIgnoreCase() > String will be compared by ignoring case sensitivity
+	 * compareTo()        > Values String compared to char by char one by one.
+	 * 
+	 * 
+	 * 
+	 */
+
+}
+
+// On the Existing memory created we cant change anything. if we try to change that change will happen in new memory
+class ImutableString{
+	
+	// 3-ways to create
+	String a = "Taj";
+	String b = new String("Taj");
+	char[] c = {'a','b','c'};
+	
+	//   If we try to change the object that change would not happen in the same memory
+	//   the changes will happen in the new memory is called  IMMUTABLE STRING
+	 
+}
